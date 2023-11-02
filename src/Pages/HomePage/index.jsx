e5cae9d236d1d28869d71d92c0aa912499140b46
SahibovaElmira <sahibovaelmira@gmail.com>
2023-11-02 22:52:27 +0300

    Stylishly created and added to HomePage

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -3,6 +3,7 @@ import Navigation from '../../Components/Navigation';
 import Banner from '../../Components/Banner';
 import WorkPlanCards from '../../Components/WorkPlan';
 import { Helmet } from 'react-helmet';
+import Stylishly from '../../Components/Stylishly';
 
 
 const HomePage = () => {
@@ -14,6 +15,7 @@ const HomePage = () => {
       <Navigation/>
       <Banner/>
       <WorkPlanCards/>
+      <Stylishly/>
     </div>
   )
 }
