7263d12e660e68bd172df01325a4bd539104cdfa
SahibovaElmira <sahibovaelmira@gmail.com>
2023-11-09 18:38:10 +0300

    Create Customer and Customers components

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
-import Navigation from '../../Components/Navigation';
-import Banner from '../../Components/Banner';
-import WorkPlanCards from '../../Components/WorkPlan';
-import { Helmet } from 'react-helmet';
-import Stylishly from '../../Components/Stylishly';
+// import React from 'react';
+// import Navigation from '../../Components/Navigation';
+// import Banner from '../../Components/Banner';
+// import WorkPlanCards from '../../Components/WorkPlan';
+// import { Helmet } from 'react-helmet';
+// import Stylishly from '../../Components/Stylishly';
 
+
+// const HomePage = () => {
+//   return (
+//     <div>
+//       <Helmet>
+//         <title>Interno</title>
+//       </Helmet>
+//       <Navigation/>
+//       <Banner/>
+//       <WorkPlanCards/>
+//       <Stylishly/>
+//     </div>
+//   )
+// }
+
+// export default HomePage
+import React from "react";
+import Navigation from "../../Components/Navigation";
+import Banner from "../../Components/Banner";
+import WorkPlanCards from "../../Components/WorkPlanCards";
+import { motion } from "framer-motion";
+import { Helmet } from "react-helmet";
+import Stylishly from "../../Components/Stylishly";
+import Customers from "../../Components/Customers";
 
 const HomePage = () => {
   return (
-    <div>
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
       <Helmet>
         <title>Interno</title>
       </Helmet>
-      <Navigation/>
-      <Banner/>
-      <WorkPlanCards/>
-      <Stylishly/>
-    </div>
-  )
-}
+      <Navigation />
+      <Banner />
+      <WorkPlanCards />
+      <Stylishly />
+      <Customers />
+    </motion.div>
+  );
+};
 
-export default HomePage
+export default HomePage;
